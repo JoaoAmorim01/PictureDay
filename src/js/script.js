53717ee7148720e93fae4617d78090fe844aceac
JoaoAmorim01 <joaovrosa10@gmail.com>
2023-07-21 22:39:23 -0300

    Javascript & API

---
+++ src/js/script.js
@@ -0,0 +1,24 @@
+const btnEnviar = document.querySelector("#enviar");
+
+btnEnviar.addEventListener("click", ()=>{
+    console.log("O botão foi pressionado");
+    sendApiRequest();
+})
+
+async function sendApiRequest(){
+    let API_KEY = "nzBLbeJQErwnqPDVhVImQD31bMSr8bCa4Ooa3TDB";
+    let input = document.querySelector("#inputData").value;
+    let response = await fetch (`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&date=${input}`);
+    let data = await response.json();
+    console.log(data);
+    useApiData(data);
+}
+
+function useApiData(data){
+    // document.querySelector("#receiveImg").innerHTML = data.explanation
+    document.querySelector("#receiveImg").innerHTML = `<img src="${data.url}">`
+}
+
+
+
+
